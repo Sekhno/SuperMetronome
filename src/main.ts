fbbe52797e962dd4a6a5f35d9d505e7d87178eb7
Dmytro Sekhno <dmytro.sekhno@semantrum.net>
2024-11-03 12:53:27 +0200

    Add analytics #40

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
 import {AppMobileComponent} from './app/app.mobile.component';
 import {AudioService} from './app/core/services/audio.service';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {environment} from "./environments/environment";
 
 const isMobile = window.matchMedia("(pointer: coarse)").matches;
 const rootComponent = isMobile ? AppMobileComponent : AppComponent;
 
-bootstrapApplication(rootComponent , {
+const app = initializeApp(environment.firebaseConfig);
+const analytics = getAnalytics(app);
 
+bootstrapApplication(rootComponent , {
   providers: [
     {
       provide: AudioService, useClass: AudioService
